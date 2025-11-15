473beb7fe72e3fefc75eca4740d7c6b106a8018b
shivusoumya <shivadatacamp@gmail.com>
2025-11-15 16:32:10 +0530

    add movies and list movies

---
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -0,0 +1,82 @@
+// let person = {
+//     name: 'Shivanand',
+//     age: 33,
+//     hobies: ['Sports', 'Cooking'],
+//     greet: function(){
+//         alert('Hi Their!');
+//     }
+// };
+
+// let personNew = {
+//     name: 'Shivanand',
+//     age: 33,
+//     hobies: ['Sports', 'Cooking'],
+//     greet: function(){
+//         alert('Hi Their!');
+//     },
+//     isAdmin: true
+// };
+
+// person.isAdmin = true;
+// console.log(person.isAdmin)
+// // person.greet();
+
+const addMovieButton = document.getElementById('add-movie-btn');
+const searchButton = document.getElementById('search-btn');
+
+const movies = [];
+
+const renderMovies = (filter = '') => {
+    const movieList = document.getElementById('movie-list');
+    
+    if(movies.length === 0){
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible');
+    }
+    movieList.innerHTML = '';  
+    
+    const filterMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter));
+    movies.forEach((movie) => {
+        const movieEl = document.createElement('li');
+        // movieEl.textContent = movie.info.title;
+        let text = movie.info.title + ' - ';
+        for(const key in movie.info) {
+            if(key !== 'title') {
+                text =text + `${key}: ${movie.info[key]}`
+            }
+        }
+        movieEl.textContent = text;
+        movieList.append(movieEl);
+    })
+}
+const addMovieHandler = () => {
+    const title = document.getElementById('title').value;
+    const extraName = document.getElementById('extra-name').value;
+    const extraValue = document.getElementById('extra-value').value;
+
+    if (title.trim() === '' || extraName.trim() === '' || extraValue.trim() === '') {
+        return;
+    }
+
+    const newMovie = {
+       info: {
+        title,
+        [extraName]: extraValue
+       },
+       id: Math.random()
+    };
+
+    movies.push(newMovie);
+    renderMovies();
+};
+
+const serachMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovies(filterTerm);
+
+}
+
+addMovieButton.addEventListener('click', addMovieHandler);
+
